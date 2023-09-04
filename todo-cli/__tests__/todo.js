b48038ebfb8ac4ccfcd35f2e7bb98e6b41dabe9f
meghanareddy <meghanareddy@DESKTOP-4LS13FK>
2023-09-04 23:14:51 +0530

    level 4 commit

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,6 +1,6 @@
 const todoList =  require('../todo');
 
-const {all, markAsComplete, add, overdues, dueToday,dueLater } = todoList();
+const {all, markAsComplete, add, overdue, dueToday,dueLater } = todoList();
 
 describe("Todolist Test Suite", () => {
     beforeAll(() => {
@@ -28,20 +28,45 @@ describe("Todolist Test Suite", () => {
         markAsComplete(0);
         expect(all[0].completed).toBe(true);
     })
-    test("Should retrieve overdue items", () => {
-        for (let i = 0; i < all.length; i++) {
-            expect(all[i].overdues < new Date().toISOString().slice(0, 10)).toBe(false);
-           //expect(overdue() > dueDate).toBe(true)
-        }
-    })
-    test("Should retrieve due today items", () => {
-        for (let i = 0; i < all.length; i++) {
-            expect(all[i].dueDate === new Date().toISOString().slice(0, 10)).toBe(true);
-        }
-    })
-    test("Should retrieve due later items", () => {
-        for (let i = 0; i < all.length; i++) {
-            expect((all[i].dueLater) < new Date().toISOString().slice(0, 10)).toBe(false);
-        }
-    })
-})
+    test("Should retrieve overdue todo", () => {
+        const tod = new Date();
+        const yester = new Date(tod);
+        yester.setDate(tod.getDate() - 1);
+        const ystr = yester.toISOString().split("T")[0];
+        const odu = overdue().length;
+        add({
+          title: "Submit assignment",
+          dueDate: ystr,
+          completed: false,
+        });
+    
+        expect(overdue().length).toBe(odu + 1);
+
+    });
+    test("Should retrieve dueDate todo", () => {
+        const today = new Date();
+        const todaystr = today.toISOString().split("T")[0];
+        const dtd = dueToday().length;
+
+        add({
+            title: "Pay rent",
+            dueDate: todaystr,
+            completed: true,
+          });
+         expect(dueToday().length).toBe(dtd+ 1);  
+    });
+    test("Should retrieve dueLater todo", () => {
+        const tod = new Date();
+        const tom = new Date(tod);
+        tom.setDate(tod.getDate()+1);
+
+        const tomstr = tom.toISOString().split("T")[0];
+        const dldd = dueLater().length;
+    add({
+      title: "Pay electric bill",
+      dueDate: tomstr,
+      completed: false,
+    });
+    expect(dueLater().length).toBe(dldd+1);
+    });
+});
