6c0d69c4ac44516742a6825a681e50c52de57bee
meghanareddy <meghanareddy@DESKTOP-4LS13FK>
2023-09-04 23:26:35 +0530

    level4_commit

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -27,9 +27,9 @@ describe("Todolist Test Suite", () => {
         expect(all[0].completed).toBe(false);
         markAsComplete(0);
         expect(all[0].completed).toBe(true);
-    })
+    });
     test("Should retrieve overdue todo", () => {
-        const tod = new Date();
+       /*const tod = new Date();
         const yester = new Date(tod);
         yester.setDate(tod.getDate() - 1);
         const ystr = yester.toISOString().split("T")[0];
@@ -41,10 +41,21 @@ describe("Todolist Test Suite", () => {
         });
     
         expect(overdue().length).toBe(odu + 1);
-
+        */
+        var dateToday = new Date()
+        const today = dateToday
+        const yesterday =
+  new Date(new Date().setDate(dateToday.getDate() - 1))
+        
+        const tomorrow = 
+        new Date(new Date().setDate(dateToday.getDate() + 1))
+        const todoitemscount = all.length
+        const overDueCount = overdue().length
+        add({ title: 'Submit assignment', dueDate: yesterday.toLocaleDateString("en-CA"), completed: false })
+        expect(overdue().length).toBe(overDueCount + 1)
     });
     test("Should retrieve dueDate todo", () => {
-        const today = new Date();
+       /* const today = new Date();
         const todaystr = today.toISOString().split("T")[0];
         const dtd = dueToday().length;
 
@@ -54,9 +65,22 @@ describe("Todolist Test Suite", () => {
             completed: true,
           });
          expect(dueToday().length).toBe(dtd+ 1);  
-    });
+         */
+         var dateToday = new Date()
+         const today = dateToday
+         const yesterday =
+   new Date(new Date().setDate(dateToday.getDate() - 1))
+         
+         const tomorrow = 
+         new Date(new Date().setDate(dateToday.getDate() + 1))
+         const todoitemscount = all.length
+ 
+             const dueTodayCount = dueToday().length
+             add({ title: 'Service Vehicle', dueDate: today.toLocaleDateString("en-CA"), completed: false })
+             expect(dueToday().length).toBe(dueTodayCount + 1)
+        });
     test("Should retrieve dueLater todo", () => {
-        const tod = new Date();
+        /*const tod = new Date();
         const tom = new Date(tod);
         tom.setDate(tod.getDate()+1);
 
@@ -69,4 +93,18 @@ describe("Todolist Test Suite", () => {
     });
     expect(dueLater().length).toBe(dldd+1);
     });
+    */
+    var dateToday = new Date()
+    const today = dateToday
+    const yesterday =
+new Date(new Date().setDate(dateToday.getDate() - 1))
+    
+    const tomorrow = 
+    new Date(new Date().setDate(dateToday.getDate() + 1))
+    const todoitemscount = all.length
+        const dueLaterCount = dueLater().length
+        add({ title: 'File taxes', dueDate: tomorrow.toLocaleDateString("en-CA"), completed: false })
+        expect(dueLater().length).toBe(dueLaterCount + 1)
+});
+        
 });
