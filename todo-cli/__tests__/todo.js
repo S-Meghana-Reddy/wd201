0bac185a33065ac60a186562c8dee0a574c89e4b
meghanareddy <meghanareddy@DESKTOP-4LS13FK>
2023-09-13 22:03:15 +0530

    level 5

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,8 +1,29 @@
-const todoList = require("../todo");
+/* eslint-disable no-undef */
+/*const db = require("../models");
 
+describe("Todolist Test Suite", () => {
+  beforeAll(async () => {
+    await db.sequelize.sync({ force: true });
+  });
+
+  test("Should add new todo", async () => {
+    const todoItemsCount = await db.Todo.count();
+    await db.Todo.addTask({
+      title: "Test todo",
+      completed: false,
+      dueDate: new Date(),
+    });
+    const newTodoItemsCount = await db.Todo.count();
+    expect(newTodoItemsCount).toBe(todoItemsCount + 1);
+  });
+});
+*/
+
+const todoList = require("../todo");
 const{all, markAsComplete, add, overdue,dueToday, dueLater} = todoList();
 
 describe("Todolist Test Suite", () => {
+    // eslint-disable-next-line no-undef
     beforeAll(() => {
         const today=new Date();
         const tomorrow = new Date(new Date().setDate(today.getDate()+1));
